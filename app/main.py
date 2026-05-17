63f7bde83483647909ef97f92dc81eef2d033f38
Maha Ammar <mahaammar93@gmail.com>
2026-05-17 09:24:28 +0100

    feat: configure Prometheus metrics and add prometheus-client dependency

--- app/main.py
+++ app/main.py
@@ -3,12 +3,24 @@ from datetime import datetime
 import socket
 import os
 
+# Prometheus
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
+
 app = FastAPI()
 
 PORT = int(os.getenv("PORT", 8000))
 
+# Metrics
+REQUEST_COUNT = Counter(
+    "app_requests_total",
+    "Total number of requests"
+)
+
 @app.get("/")
 def home():
+    REQUEST_COUNT.inc()
+
     return {
         "message": "Simple API running",
         "hostname": socket.gethostname(),
@@ -18,13 +30,25 @@ def home():
 
 @app.get("/health")
 def health():
+    REQUEST_COUNT.inc()
+
     return {
         "status": "healthy"
     }
 
 @app.get("/users")
 def users():
+    REQUEST_COUNT.inc()
+
     return [
         {"id": 1, "name": "Maha"},
         {"id": 2, "name": "DevOps User"}
     ]
+
+# Endpoint Prometheus
+@app.get("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        media_type="text/plain"
+    )
